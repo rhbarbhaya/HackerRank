bd376b0ca652caf3d7ba088cbe18a38cb2ddeafb
rhbarbhaya <rhbarbhaya@gmail.com>
2020-12-27 16:48:38 -0700

    init

---
+++ Python/Problem Solving/Number Line Jumps.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the kangaroo function below.
+def kangaroo(x1, v1, x2, v2):
+    jumps = 0
+    while jumps < 10000:
+        if x1 + jumps*v1 == x2 + jumps*v2:
+            return("YES")
+        jumps += 1
+    return("NO")
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    x1V1X2V2 = input().split()
+
+    x1 = int(x1V1X2V2[0])
+
+    v1 = int(x1V1X2V2[1])
+
+    x2 = int(x1V1X2V2[2])
+
+    v2 = int(x1V1X2V2[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
